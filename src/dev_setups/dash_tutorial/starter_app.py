aaed4af04321316078bd69b2efaba518b8281f13
Bernard Roesler <bernard.roesler@gmail.com>
2018-06-09 07:32:53 -0400

    Initial commit.

---
+++ src/dev_setups/dash_tutorial/starter_app.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python3
+#==============================================================================
+#     File: starter_app.py
+#  Created: 06/08/2018, 14:38
+#   Author: Bernie Roesler
+#
+"""
+  Description: Dash developer setup.
+"""
+#==============================================================================
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+#==============================================================================
+#==============================================================================

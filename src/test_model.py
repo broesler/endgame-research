b13c9238b9e5cb5769ebd0f338b9cf0b4a674f1e
Bernard Roesler <bernard.roesler@gmail.com>
2018-06-21 17:36:02 -0400

    Upsample minority classes properly and remake ROC curves per class.

--- src/test_model.py
+++ src/test_model.py
@@ -17,36 +17,77 @@ from mpl_toolkits.mplot3d import axes3d
 from scipy import interp
 import seaborn as sns
 
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, label_binarize
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report, roc_curve
-
-from cb_funcs import Nstats
+from sklearn.metrics import classification_report, roc_curve, auc
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.utils import resample
 
 # Equivalent to "shortg" Matlab format
 np.set_printoptions(precision=4, suppress=True)
 
 plt.ion()
 
-# IMPORT THE DATA!!
-df = pd.read_pickle('../data/cb_input.pkl')
+#------------------------------------------------------------------------------ 
+#        IMPORT THE DATA!!
+#------------------------------------------------------------------------------
+df = pd.read_pickle('../data/cb_input_multi.pkl')
 
 feat_cols = ['age_at_exit', 'milestones', 'latitude', 'longitude', 'offices',
              'products', 'funding_rounds', 'investment_rounds',
              'invested_companies', 'acq_before_exit', 'investors',
-             'investors_per_round', 'funding_per_round', 'experience']
-labels = ['success', 'failure']
-
-X = df[feat_cols]
-# y = df[labels]
-y = df['bin_label']
+             'investors_per_round', 'funding_per_round', 'avg_time_to_funding',
+             'experience', 'a', 'angel', 'b', 'c', 'convertible', 'crowd',
+             'crowd_equity', 'd', 'debt_round', 'e', 'f', 'g', 'grant',
+             'partial', 'post_ipo_debt', 'post_ipo_equity', 'private_equity',
+             'secondary_market', 'seed', 'unattributed']
 
 #------------------------------------------------------------------------------ 
-#        Data Statistics
+#        Upsample minority classes
 #------------------------------------------------------------------------------
-# Nt = Nstats(X, y)
+# >>> df.label.value_counts()
+# 3.0    16904
+# 1.0     2103
+# 0.0     1178
+# 2.0      683
+# Name: label, dtype: int64
 
+df_maj = df[df.label == 3] # majority class
+df_min_u = []
+for i in range(3):
+    # Upsample minority class
+    df_min_u.append(resample(df[df.label == i], 
+                           replace=True, 
+                           n_samples=df_maj.shape[0],
+                           random_state=56))
+
+# Combine majority class with upsampled minority class
+df_up = pd.concat([df_maj] + df_min_u)
+
+df = df_up # use resampled data!
+
+# Display new class counts
+# df_up.label.value_counts()
+# 1.0    16904
+# 2.0    16904
+# 0.0    16904
+# 3.0    16904
+# Name: label, dtype: int64
+
+#------------------------------------------------------------------------------ 
+#        Extract features and labels
+#------------------------------------------------------------------------------
+X = df[feat_cols]
+
+# Binarize the labels
+y = df.label
+yb = label_binarize(y, classes=[0, 1, 2, 3])
+y = pd.DataFrame(data=yb, index=y.index)
+
+#------------------------------------------------------------------------------ 
+#        Normalize the data
+#------------------------------------------------------------------------------
 # Impute NaN values to mean of column
 X = X.fillna(X.median())
 
@@ -54,48 +95,44 @@ X = X.fillna(X.median())
 Xn = StandardScaler().fit_transform(X)
 Xn = pd.DataFrame(data=Xn, columns=X.columns, index=X.index)
 
-# Test/Train Split
+# Train/Test Split -- "_test" == HOLDOUT
 X_train, X_test, y_train, y_test = train_test_split(Xn, y, 
-                                                    train_size=0.6, 
+                                                    train_size=0.9, 
                                                     stratify=y,
                                                     random_state=56)
 
-# N_train = Nstats(X_train, y_train)
-# N_test = Nstats(X_test, y_test)
-
-# # Perform PCA decomposition to see n most important stats
-# U, sigma, V = np.linalg.svd(Xn.T) # sigma is shape (m,) array, NOT matrix
-#
-# m = sigma.shape[0]  # number of singular values
-# k = 6               # number of dimensions in subspace (i.e. PCs to keep)
-#
-# explained_variance_ratio = sigma / sum(sigma)
-#
-# thresh = 0.8
-# idx_sig = np.argmax(np.cumsum(explained_variance_ratio) >= thresh)
-# sigma_allowed = sigma[:idx_sig]
+# Split into training/cross-validation 
+X_train, X_cv, y_train, y_cv = train_test_split(X_train, y_train, 
+                                                train_size=0.6, 
+                                                stratify=y_train,
+                                                random_state=56)
 
 #------------------------------------------------------------------------------ 
 #        Train the Model
 #------------------------------------------------------------------------------
-# rfc = RandomForestClassifier(n_estimators=10, class_weight='balanced')
-rfc = RandomForestClassifier(n_estimators=10).fit(X_train, y_train)
-# rfc = rfc.fit(X_train, y_train)
+clf = RandomForestClassifier(min_samples_split=50)
+clf = clf.fit(X_train, y_train)
 
-pred_train = rfc.predict(X_train)
+pred_train = clf.predict(X_train)
 pred_train = pd.DataFrame(data=pred_train,
                           index=y_train.index)
 print(classification_report(y_train, pred_train))
 
-pred_test = rfc.predict(X_test)
+pred_cv = clf.predict(X_cv)
+pred_cv = pd.DataFrame(data=pred_cv,
+                          index=y_cv.index)
+print(classification_report(y_cv, pred_cv))
+
+pred_test = clf.predict(X_test)
 pred_test = pd.DataFrame(data=pred_test,
                          index=y_test.index)
-print(classification_report(y_test, pred_test))
+# print(classification_report(y_test, pred_test))
 
-pred = rfc.predict(Xn)
-pred = pd.DataFrame(data=pred, index=y.index)
+# Predict ALL outcomes
+# pred = clf.predict(Xn)
+# pred = pd.DataFrame(data=pred, index=y.index)
 
-# Confusion matrix -- need to convert back to single value encoding
+# TODO Confusion matrix -- need to convert back to single value encoding
 # cm = pd.DataFrame(confusion_matrix(y_test, pred_test).T,
 #                   index=['Yes', 'No'],
 #                   columns=['Yes', 'No'])
@@ -106,32 +143,68 @@ pred = pd.DataFrame(data=pred, index=y.index)
 #------------------------------------------------------------------------------ 
 #        Feature Importance
 #------------------------------------------------------------------------------
-fp = dict(zip(feat_cols, rfc.feature_importances_))
+fp = dict(zip(feat_cols, clf.feature_importances_))
 fp = [(k, fp[k]) for k in sorted(fp, key=fp.get, reverse=True)]
 
 #------------------------------------------------------------------------------ 
 #        ROC Curves
 #------------------------------------------------------------------------------
-tprs = []
-mean_fpr = np.linspace(0, 1, 100)
+probas_ = clf.predict_proba(X_test)
 
-probas_ = rfc.predict_proba(X_test)
-# Compute ROC curve and area the curve
-fpr, tpr, _ = roc_curve(y_test, probas_[:, 1])
-tprs.append(interp(mean_fpr, fpr, tpr))
-tprs[-1][0] = 0.0
+# Compute ROC curve and ROC area for each class
+n_classes = y.shape[1]
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test.loc[:, i], probas_[i][:, 1])
+    roc_auc[i] = auc(fpr[i], tpr[i])
 
+# Compute micro-average ROC curve and ROC area
+# fpr['micro'], tpr['micro'], _ = roc_curve(y_test.ravel(), y_score.ravel())
+# roc_auc['micro'] = auc(fpr['micro'], tpr['micro'])
+
+# Compute macro-average ROC curve and ROC area
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr['macro'] = all_fpr
+tpr['macro'] = mean_tpr
+roc_auc['macro'] = auc(fpr['macro'], tpr['macro'])
+
+# Plot all ROC curves
 plt.figure(1)
 plt.clf()
-plt.plot(fpr, tpr, label='Random Forest')
-plt.plot([0, 1], [0, 1], 
-         linestyle='--', lw=2, color='k', alpha=0.8, 
-         label='Pure Chance')
 
+# plt.plot(fpr['micro'], tpr['micro'],
+#          label='micro-average ROC curve (area = {0:0.2f})'
+#                ''.format(roc_auc['micro']),
+#          color='deeppink', linestyle=':', linewidth=4)
+
+classes = ['Failed', 'Timely Exit', 'Late Exit', 'Steady Operation']
+for i in range(n_classes):
+    plt.plot(fpr[i], tpr[i], 
+             label="{} (AUC = {:0.2f})".format(classes[i], roc_auc[i]))
+
+plt.plot(fpr['macro'], tpr['macro'],
+         label="Macro-average (AUC = {:0.2f})".format(roc_auc['macro']),
+         color='navy', linestyle='-.')
+
+plt.plot([0, 1], [0, 1], 'k--', lw=1) # random chance
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic (ROC) Curve')
-plt.legend(loc='lower right')
+plt.title('Multi-class Receiver Operating Characteristic')
+plt.legend()
+plt.tight_layout()
+plt.show()
 
 #==============================================================================
 #==============================================================================
--- src/timeseries_features.py
+++ src/timeseries_features.py
@@ -98,86 +98,39 @@ df = df[df.id.isin(fund_cv.id.unique())]
 #------------------------------------------------------------------------------ 
 #        Prepare DataFrame for lifelines analysis
 #------------------------------------------------------------------------------
-lf = to_long_format(df, 'age_at_exit')
-
-# Piped version (possibly faster?)
-# lf = df.pipe(to_long_format, 'age_at_exit')\
-#        .pipe(add_covariate_to_timeline(fund_cv_amt, 
-#                                        'id', 'time_to_funding', 'success',
-#                                        cumulative_sum=False)\
-
-# NOTE WARNING THIS LINE IS SUPER FUCKING SLOW.
-# Add raised_amount_usd as time-varying covariate
-fund_cv_amt = fund_cv[['id', 'raised_amount_usd', 'time_to_funding']]
-lf = add_covariate_to_timeline(lf, fund_cv_amt, 'id', 'time_to_funding', 'success',
-                               cumulative_sum=False)
-
-# Add cumulative funding as covariate
-# lf = add_covariate_to_timeline(lf, fund_cv_amt, 
-#                                'id', 'time_to_funding', 'success',
-#                                cumulative_sum=True)
-
-# Add funding round type as covariate
+# lf = to_long_format(df, 'age_at_exit')
 #
-# lf = pd.read_pickle('../data/survival_input_clean.pkl')
+# # Piped version (possibly faster?)
+# # lf = df.pipe(to_long_format, 'age_at_exit')\
+# #        .pipe(add_covariate_to_timeline(fund_cv_amt, 
+# #                                        'id', 'time_to_funding', 'success',
+# #                                        cumulative_sum=False)\
 #
-# # Drop rows where start == stop == 0
-# lf = lf.loc[~((lf.start == lf.stop) & (lf.start == 0))]
-# lf = lf.loc[~((lf.start < 0) | (lf.stop < 0))]
+# # NOTE WARNING THIS LINE IS SUPER FUCKING SLOW.
+# # Add raised_amount_usd as time-varying covariate
+# fund_cv_amt = fund_cv[['id', 'raised_amount_usd', 'time_to_funding']]
+# lf = add_covariate_to_timeline(lf, fund_cv_amt, 'id', 'time_to_funding', 'success',
+#                                cumulative_sum=False)
 #
-# # Cut off study at t = 15 years
-# lf.loc[lf.stop > 15*365, 'stop'] = 15*365
-# lf.start.fillna(value=0, inplace=True)
-# lf.fillna(value=lf.median(), inplace=True)
-#
-# # Randomly take small sample for testing
-# # test = lf.loc[lf.id.isin(np.random.choice(lf.id, size=1000))]
-#
-# # lr = lf[(lf.name == 'Twitter') | (lf.name == 'Facebook')]
+# # Add cumulative funding as covariate
+# # lf = add_covariate_to_timeline(lf, fund_cv_amt, 
+# #                                'id', 'time_to_funding', 'success',
+# #                                cumulative_sum=True)
+# # Add funding round type as covariate
 #
 # # WRITE TO PICKLE FILE!!!
 # # lf.to_pickle('../data/survival_input.pkl')
 #
-# # Using Cox Proportional Hazards model
-# # cph = CoxPHFitter()
-# # drop_cols = ['category_code', 'name', 'id', 'founded_at']
-# # lf_n = test.drop(columns=drop_cols)
-# # cph.fit(lf_n, duration_col='stop', event_col='success')
-# # cph.print_summary()
-# # cph.plot()
+# # df_t = fund_cv[fund_cv.name == 'Twitter']
+# # df_f = fund_cv[fund_cv.name == 'Facebook']
+# # fig = plt.figure(9)
+# # plt.clf()
+# # ax = plt.gca()
+# # plt.plot(df_t.time_to_funding, df_t.raised_amount_usd, '-xb', markersize=10)
+# # plt.plot(df_f.time_to_funding, df_f.raised_amount_usd, '-xr', markersize=10)
+# # ax.set_yscale('log')
+# # plt.tight_layout()
+# # plt.show()
 #
-# # lf_n = test.copy()
-# # lf_n.start.fillna(value=0, inplace=True)
-# # lf_n.fillna(value=test.median(), inplace=True)
-# ctv = CoxTimeVaryingFitter()
-# ctv.fit(lf, id_col='id', event_col='success', start_col='start', stop_col='stop',
-#         show_progress=True, step_size=0.1)
-# ctv.print_summary()
-# ctv.plot()
-#
-# # Fit the survival curve
-# kmf = KaplanMeierFitter()
-# kmf.fit(durations=lf_n.stop, event_observed=lf_n.success)  # or, more succiently, kmf.fit(T, E)
-# kmf.plot()
-#
-# aaf = AalenAdditiveFitter(fit_intercept=False)
-# aaf.fit(lf_n, duration_col='stop', event_col='success')
-# aaf.plot()
-
-# X = lf_n.drop(['success', 'stop'], axis=1)
-# aaf.predict_survival_function(X.iloc[10:12]).plot()  # get the unique survival functions of two subjects
-
-# df_t = fund_cv[fund_cv.name == 'Twitter']
-# df_f = fund_cv[fund_cv.name == 'Facebook']
-# fig = plt.figure(9)
-# plt.clf()
-# ax = plt.gca()
-# plt.plot(df_t.time_to_funding, df_t.raised_amount_usd, '-xb', markersize=10)
-# plt.plot(df_f.time_to_funding, df_f.raised_amount_usd, '-xr', markersize=10)
-# ax.set_yscale('log')
-
-plt.tight_layout()
-plt.show()
-
-#==============================================================================
-#==============================================================================
+# #==============================================================================
+# #==============================================================================
